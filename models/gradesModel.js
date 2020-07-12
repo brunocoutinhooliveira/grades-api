c19d8618fa3018394d49d6ed36bab0e5942c135f
brunodba <brunodba@live.com>
2020-07-12 05:02:40 -0300

    Adicionado projeto clone para frontend.

---
+++ models/gradesModel.js
@@ -0,0 +1,33 @@
+export default (mongoose) => {
+  const schema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  });
+
+  schema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+
+    object.id = _id;
+
+    return object;
+  });
+
+  const Grade = mongoose.model('grades', schema);
+
+  return Grade;
+};
