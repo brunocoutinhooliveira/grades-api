4b66bdb0f082455c8b4124da3eb84f92d66105d7
brunodba <brunodba@live.com>
2020-07-12 06:52:36 -0300

    Ajustado endereço do app.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://boiling-falls-09659.herokuapp.com/',
+    origin: 'https://front-grade-bruno.herokuapp.com/',
   })
 );
 
